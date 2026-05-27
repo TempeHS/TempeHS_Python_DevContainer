5ebea12539364c64bde152e04cbf7176026d5fbe
Zoë R <126206627+ThatOneArtKid@users.noreply.github.com>
2026-05-27 15:06:12 +1000

    continued w/ mechanics

--- game/main.py
+++ game/main.py
@@ -47,7 +47,6 @@ class mouse(pg.sprite.Sprite):
         self.punching = False
 
     def update(self):
-        """move the fist based on the mouse position"""
         pos = pg.mouse.get_pos()
         self.rect.topleft = pos
         self.rect.move_ip(self.fist_offset)
@@ -55,14 +54,12 @@ class mouse(pg.sprite.Sprite):
             self.rect.move_ip(15, 25)
 
     def punch(self, target):
-        """returns true if the fist collides with the target"""
         if not self.punching:
             self.punching = True
             hitbox = self.rect.inflate(-5, -5)
             return hitbox.colliderect(target.rect)
 
     def unpunch(self):
-        """called to pull the fist back"""
         self.punching = False
 
 #they lobotomised youareanidiot :[
@@ -78,14 +75,12 @@ class smiley(pg.sprite.Sprite):
         self.dizzy = False
 
     def update(self):
-        """walk or spin, depending on the monkeys state"""
         if self.dizzy:
             self._spin()
         else:
             self._walk()
 
     def _walk(self):
-        """move the monkey across the screen, and turn at the ends"""
         newpos = self.rect.move((self.move, 0))
         if not self.area.contains(newpos):
             if self.rect.left < self.area.left or self.rect.right > self.area.right:
@@ -96,7 +91,6 @@ class smiley(pg.sprite.Sprite):
 
 
     def _spin(self):
-        """spin the monkey image"""
         center = self.rect.center
         self.dizzy = self.dizzy + 12
         if self.dizzy >= 360:
@@ -108,7 +102,53 @@ class smiley(pg.sprite.Sprite):
         self.rect = self.image.get_rect(center=center)
 
     def punched(self):
-        """this will cause the monkey to start spinning"""
         if not self.dizzy:
             self.dizzy = True
             self.original = self.image
+
+#initialise pygame
+pg.init()
+#set screen size
+screen = pg.display.set_mode((1280, 480), pg.SCALED)
+pg.display.set_caption("test game")
+pg.mouse.set_visible(False)
+
+background = pg.Surface(screen.get_size())
+background = background.convert()
+background.fill((170,238,17))
+
+if pg.font:
+    font = pg.font.Font(None, 64)
+    text = font.render("game", True, (10,10,10))
+    textpos = text.get_Rect(centerx=background.get_width()/2, y=10)
+    background.blit(text, textpos)
+0
+screen.blit(background, (0,0))
+pg.display.flip()
+
+mouse = mouse()
+smiley = smiley()
+allsprites = pg.sprite.RenderPlain((mouse, smiley))
+clock = pg.time.Clock()
+
+going = True
+while going:
+    clock.tick(60)
+
+for event in pg.event.get():
+    if event.type == pg.QUIT:
+        going = False
+    elif event.type == pg.KEYDOWN and event.key == pg.ESCAPE:
+        going = False
+    elif event.type == pg.MOUSEBUTTONDOWN:
+        if mouse.punch(smiley):
+            smiley.punched
+    elif event.type == pg.MOUSEBUTTONUP:
+        mouse.unpunch
+    allsprites.update()
+
+screen.blit(background, (0, 0))
+allsprites.draw(screen)
+pg.display.flip()
+
+pg.quit()
